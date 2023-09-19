737787a4a72080439d3451971b454abe6fecd474
nhollain <N.Hollain@student.science.ru.nl>
2023-09-19 15:25:23 +0200

    Fixed GPU functioning, made table of captioning results

--- models.py
+++ models.py
@@ -3,9 +3,9 @@ import torch
 import gc
 
 class Captioning_Model():
-    def __init__(self, model_type:str = 'blip'):
+    def __init__(self, model_type:str = 'blip', gpu = True):
         self.model_type = model_type
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() and gpu else "cpu")
         if 'vit' == model_type:
             self.path = "nlpconnect/vit-gpt2-image-captioning"
             self.model = transformers.VisionEncoderDecoderModel.from_pretrained(self.path)
@@ -27,31 +27,24 @@ class Captioning_Model():
             print('Failed to make model GPU compatible. Running on CPU.')
             self.device = 'cpu'
             self.model.to(self.device)
-    def predict(self, images, text = None, log = False): 
+    def predict(self, images, log = False): # text = None, 
         if log:
             print('Predicting...')
         if type(images) != list:
             # print('You need to provide a list of images. If you have one image, input it as: [image].')
             images = [images]
         inputs = self.processor(images=images, return_tensors="pt").pixel_values.to(self.device)
-        if 'vit' == self.model_type:
-            max_length = 16
-            num_beams = 4
-            gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
-            output = self.model.generate(inputs, **gen_kwargs)
+        gen_kwargs = {'vit': {"max_length": 16, "num_beams": 4}, 'blip': {"max_length": 20}, 'git': {"max_length": 50}}
+        output = self.model.generate(pixel_values = inputs, **gen_kwargs[self.model_type])
+        if 'vit' == self.model_type: 
             preds = self.tokenizer.batch_decode(output, skip_special_tokens=True)
             preds = [pred.strip() for pred in preds]
-        elif 'blip' == self.model_type:
-            inputs = self.processor(images, text, return_tensors="pt").pixel_values.to(self.device)
-            # print(**inputs) # , type(inputs), len(inputs)
-            output = self.model.generate(inputs)
-            preds = self.processor.batch_decode(output, skip_special_tokens=True)
-        elif 'git' == self.model_type:
-            gen_kwargs = {"max_length": 50}
-            output = self.model.generate(pixel_values = inputs, **gen_kwargs)
+        else:
+            # inputs = self.processor(images, text, return_tensors="pt").pixel_values.to(self.device)
             preds = self.processor.batch_decode(output, skip_special_tokens=True)
         if log:
             print('{} prediction(s): {}'.format(self.model_type, preds))
+        del inputs
         inputs = None
         torch.cuda.empty_cache()
         gc.collect()
