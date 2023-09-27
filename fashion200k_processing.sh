517f3169f5855ca67295d58f659db0016f9de457
nhollain <N.Hollain@student.science.ru.nl>
2023-09-27 11:07:21 +0200

    Fixed a suboptimal part of processing code

--- fashion200k_processing.sh
+++ fashion200k_processing.sh
@@ -2,7 +2,7 @@
 root_directory="$1"
 
 # Loop through each subdirectory in the root directory
-find "$root_directory" -mindepth 2 -type d | while read -r subdirectory; do
+find "$root_directory" -mindepth 2 -maxdepth 2 -type d | while read -r subdirectory; do
     # Execute the find and mv command for each subdirectory
     echo "$subdirectory" \;
     find "$subdirectory" -mindepth 2 -type f -exec mv --backup=numbered {} "$subdirectory" \;
