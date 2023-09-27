15a649fca884777bb1ff4739706b4ca4b2597483
nhollain <N.Hollain@student.science.ru.nl>
2023-09-27 11:00:05 +0200

    Adding fashion data processing

---
+++ fashion200k_processing.sh
@@ -0,0 +1,11 @@
+# Run as: bash fashion200k_processing.sh ".\data\Fashion200k\women"
+root_directory="$1"
+
+# Loop through each subdirectory in the root directory
+find "$root_directory" -mindepth 2 -type d | while read -r subdirectory; do
+    # Execute the find and mv command for each subdirectory
+    echo "$subdirectory" \;
+    find "$subdirectory" -mindepth 2 -type f -exec mv --backup=numbered {} "$subdirectory" \;
+done
+
+find "$root_directory" -mindepth 3 -type d -delete;
