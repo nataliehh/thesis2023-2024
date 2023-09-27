134b0aed7e6ab0251f29ad65377184e4f36e6952
nhollain <N.Hollain@student.science.ru.nl>
2023-09-27 11:16:36 +0200

    Adding some comments to the shell script

--- fashion200k_processing.sh
+++ fashion200k_processing.sh
@@ -1,11 +1,18 @@
 # Run as: bash fashion200k_processing.sh ".\data\Fashion200k\women"
 root_directory="$1"
 
-# Loop through each subdirectory in the root directory
+# The structure of the fashion data is as follows: 
+# The root directory is /women/, from which we get to folders with the main clothing groups (e.g. dresses, pants)
+# Within each main clothing group, the items are split into more refined categories (e.g. a dress can be a gown, cocktail dress...)
+# Finally, each item has its own folder with an associated item ID, e.g. a specific cocktail dress may have folder 123456
+# Then, the folder path to the images of this particular cocktail dress looks something like: /women/dresses/cocktail/123456
+
+# Loop through each main category (e.g. dresses) in the root directory (/women/)
 find "$root_directory" -mindepth 2 -maxdepth 2 -type d | while read -r subdirectory; do
-    # Execute the find and mv command for each subdirectory
     echo "$subdirectory" \;
+    # Find the ID folders (e.g. 123456) and move the images from there to the parent folder (e.g. cocktail dress)
     find "$subdirectory" -mindepth 2 -type f -exec mv --backup=numbered {} "$subdirectory" \;
 done
 
+# Remove all ID folders (e.g. 123456) that are now empty after the move
 find "$root_directory" -mindepth 3 -type d -delete;
