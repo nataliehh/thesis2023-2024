36956020ff0ed4210ba72ea2cfcfb39745e13883
nhollain <N.Hollain@student.science.ru.nl>
2023-11-07 15:51:53 +0100

    Cleaning up files, adding test set params

--- unzip_files.py
+++
@@ -1,5 +0,0 @@
-import zipfile
-zip_file = 'CNGT_isolated_signers' #'Signbank-NGT-videos.zip'
-path = '/vol/tensusers5/nhollain/'
-with zipfile.ZipFile(path + zip_file + '.zip', 'r') as zip_ref:
-    zip_ref.extractall(path + zip_file)
